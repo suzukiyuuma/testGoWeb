a4346beacdfc09455706605ecd4197ad7b2a0023
vagrant <vagrant@localhost.localdomain>
2023-09-23 00:18:35 +0900

    accept parameters

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # サーバー立ち上げ方法
 下記リンクをクリック
-http://192.168.33.10:8080/
+http://192.168.33.10:8080?name=Renk-Suzuki
+
+curl -X POST "http://192.168.33.10:8080?name=Renk-Suzuki" -d "name=Renk-Suzuki"
--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import "fmt"
 import "net/http"
 
-func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hello World\n")
-	fmt.Fprintf(w, "Hello World\n")
-	fmt.Fprintf(w, "Hello World\n")
-	fmt.Fprintf(w, "Hello World\n")
-	fmt.Fprintf(w, "Hello World\n")
-	fmt.Fprintf(w, "Hello World")
+const baseMessage = "Hello World"
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    name := r.FormValue("name")
+    if name == "" {
+        fmt.Fprint(w, baseMessage)
+        return
+    }
+
+    fmt.Fprintf(w, "%s, %s", baseMessage, name)
 }
+
 func main(){
 	fmt.Println("webサーバー立ち上げ開始")
 	http.HandleFunc("/", handler)
